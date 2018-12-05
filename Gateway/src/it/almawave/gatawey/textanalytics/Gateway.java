a4e5c0539e16560c36efd3a6835c7186765d3972
vivianaopen <Viviana OS@10.121.180.88>
2018-12-05 11:14:07 +0100

    spostati

--- Gateway/src/it/almawave/gatawey/textanalytics/Gateway.java
+++ Gateway/src/it/almawave/gatawey/textanalytics/Gateway.java
@@ -1,12 +1,8 @@
 package it.almawave.gatawey.textanalytics;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import javax.activation.DataHandler;
 import javax.ejb.EJB;
-import javax.mail.util.ByteArrayDataSource;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.transaction.SystemException;
@@ -17,19 +13,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.io.FileUtils;
 import org.jboss.logging.Logger;
 
-import it.almawave.gatawey.asr.ServiceUpload;
 import it.almawave.gateway.db.GatewayInternalDb;
 import it.almawave.gateway.db.bean.DoRequestBean;
-import it.pervoice.audiomabox.commontypes._1.FileType;
-import it.pervoice.audiomabox.services.common._1.ClientInfoType;
-import it.pervoice.audiomabox.services.upload._1.UploadRequest;
-import it.pervoice.audiomabox.services.upload._1.UploadRequest.RemoteFile;
-import it.pervoice.audiomabox.services.upload._1.UploadResponse;
-import it.pervoice.ws.audiomabox.service.upload._1.UploadFault;
-import it.pervoice.ws.audiomabox.service.upload._1.UploadWS;
 
 @Path("/service")
 public class Gateway {
@@ -55,43 +42,13 @@ public class Gateway {
 		String response = "";
 		try {
 			
-			//leggi file audio
-			File file = new File(request.getPercorsoFileAudio()); 
-	        byte[] data = FileUtils.readFileToByteArray(file);
-	        ByteArrayDataSource rawData = new ByteArrayDataSource(data,"application/octet-stream");
- 		
-	        //invocare il servizio uploadService
-	        UploadWS service = new ServiceUpload().getService(); 
-	        //request
-	        UploadRequest uploadRequest = new UploadRequest();
-	        //clientInfo
-	        ClientInfoType clientInfoType = new ClientInfoType();
-	        uploadRequest.setClientInfo(clientInfoType);
-	        //file
-	        RemoteFile remoteFile = new RemoteFile();
-	        FileType fileType = new FileType(); 
-	        fileType.setName(file.getName());
-	        DataHandler dataHandler =  new DataHandler(rawData);
-	        fileType.setData(dataHandler);
-	        remoteFile.setFile(fileType);
-	        uploadRequest.setRemoteFile(remoteFile);
-	        
-	        //TODO: da finire il popolamento della request
-	        UploadResponse uploadResponse = service.upload(uploadRequest);
-	        String id = String.valueOf(uploadResponse.getJobElement().get(0).getJobId());
-	        
-	        response = gatewayInternalDbEjb.doRequest(request, id);
-	        
-	        
-		}catch (FileNotFoundException e) {
-			return "File audio non trovato";
-		} catch (UploadFault e) {
-			e.printStackTrace();
-			return "Errore nella chiata al servizio Upload di PerVoice";
-		} finally {
+			response = gatewayInternalDbEjb.doRequest(request);
+			
+		}catch (Exception e) {
+			return "Errore " + e.getMessage();
+		}finally {
 			LOGGER.info("[Service doRequest ENDED]");
 		}
-		
 		return response;
 	}
 	
@@ -99,15 +56,15 @@ public class Gateway {
 	@GET
 	@Path("getStatus")
 	@Consumes({MediaType.APPLICATION_JSON})
-	public String getStatus(String idRichiesta) throws IOException{
+	public String getStatus(String idDifformita) throws IOException{
 		LOGGER.info("[Service getStatus INVOKED]");
 		String response = "";
 		try {
 		
-			response = gatewayInternalDbEjb.getStatus(idRichiesta);
+			response = gatewayInternalDbEjb.getStatus(idDifformita);
 		
 		}catch (Exception e) {
-			return "Errore ";
+			return "Errore " + e.getMessage();
 		}finally {
 			LOGGER.info("[Service getStatus ENDED]");
 		}
